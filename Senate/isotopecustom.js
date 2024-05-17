1d31a4e94184d939d1ce546c53e9160f3295abab
gloverd <glover.d@gmail.com>
2024-05-17 16:56:41 -0400

    Update isotopecustom.js

--- Senate/isotopecustom.js
+++ Senate/isotopecustom.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             pname: '.pname',
             fname: '.fname',
             lname: '.lname',
-            state: '[state-name]'
+            state: '.statechip'
         }
     });
 
