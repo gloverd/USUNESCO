9ce80cedc2db0f85afd7564db6a8f9a9a14d3468
gloverd <glover.d@gmail.com>
2024-05-17 15:18:19 -0400

    Update isotopecustom.js

--- Senate/isotopecustom.js
+++ Senate/isotopecustom.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
     // Initialize Isotope
     var $gallery = $('.gallery').isotope({
         itemSelector: '.card',
-        percentPosition: true,
-        masonry: {
-            columnWidth: '.card',
-            gutter: 16
+        layoutMode: 'fitRows', // Use fitRows layout mode for consistent row heights
+        fitRows: {
+            gutter: 16 // Ensure the gutter spacing is maintained
         },
         getSortData: {
             name: '[data-name]',
@@ -17,15 +16,5 @@ $(document).ready(function() {
     $('.button-group').on('click', 'button', function() {
         var sortByValue = $(this).attr('data-sort-by');
         $gallery.isotope({ sortBy: sortByValue });
-    });
-
-      // change is-checked class on buttons
-    $('.button-group').each( function( i, buttonGroup ) {
-      var $buttonGroup = $( buttonGroup );
-      $buttonGroup.on( 'click', 'button', function( event ) {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        var $button = $( event.currentTarget );
-        $button.addClass('is-checked');
-      });
     });
 });
