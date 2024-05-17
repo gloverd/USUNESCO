27d05537f387950478800d1f81e3e72403da9305
gloverd <glover.d@gmail.com>
2024-05-17 17:14:11 -0400

    Update isotopecustom.js

--- Senate/isotopecustom.js
+++ Senate/isotopecustom.js
@@ -19,4 +19,30 @@ $(document).ready(function() {
         var sortByValue = $(this).attr('data-sort-by');
         $gallery.isotope({ sortBy: sortByValue });
     });
+
+
+    // store filter for each group
+    var filters = {};
+    $('#filters').on( 'click', '.button', function( event ) {
+      var $button = $( event.currentTarget );
+      // get group key
+      var $buttonGroup = $button.parents('.button-group');
+      var filterGroup = $buttonGroup.attr('data-filter-group');
+      // set filter for group
+      filters[ filterGroup ] = $button.attr('data-filter');
+      // combine filters
+      var filterValue = concatValues( filters );
+      // set filter for Isotope
+      $grid.isotope({ filter: filterValue });
+    });
+
+        // flatten object by concatting values
+    function concatValues( obj ) {
+      var value = '';
+      for ( var prop in obj ) {
+        value += obj[ prop ];
+      }
+      return value;
+    }
+    
 });
