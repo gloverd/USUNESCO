864b1a792651d134566049e22987d994f71ebf23
gloverd <glover.d@gmail.com>
2024-05-20 12:59:48 -0400

    Update combined.js

--- Senate/combined.js
+++ Senate/combined.js
@@ -1,19 +1,5 @@
 (function($, window, document, undefined) {
 
-  // Isotope custom script initialization
-  var isotopeInit = function() {
-    var $grid = $('.grid').isotope({
-      itemSelector: '.grid-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('.filter-button-group').on('click', 'button', function() {
-      var filterValue = $(this).attr('data-filter');
-      $grid.isotope({ filter: filterValue });
-    });
-  };
-
-  // Support touch events detection
   var supportTouch = (function() {
     var test = 'ontouchstart' in window;
     if (!test && 'DocumentTouch' in window) {
@@ -21,7 +7,7 @@
     };
     return test ||
       navigator.maxTouchPoints > 0 ||
-      window.navigator.msMaxTouchPoints > 0;
+      window.navigator.msMaxTouchPoints > 0;    
   })();
 
   var touchEvents = {
@@ -29,11 +15,12 @@
     move: supportTouch ? 'touchmove' : 'mousemove',
     end: supportTouch ? 'touchend' : 'mouseup'
   };
-
+  
   var pluginName = 'abcScroll';
-
+  
   function getTarget(e) {
     var target;
+    
     if (supportTouch) {
       if (e.targetTouches.length) {
         target = document.elementFromPoint(e.targetTouches[0].clientX, e.targetTouches[0].clientY);
@@ -41,51 +28,60 @@
     } else {
       target = e.target;
     }
+
     return target;
   }
 
   function AbcScroll(container) {
     var self = this;
+
     this.$container = $(container);
+    
     this.$el = this.$container.find('.js-abcscroll--nav');
     this.$content = this.$container.find('.js-abcscroll--content');
+
     this.posList = [];
 
     this.$el.on(touchEvents.start + '.abcScroll', this.handleStart.bind(this));
-    this.$el.on(touchEvents.move + '.abcScroll', this.handleMove.bind(this));
+    this.$el.on(touchEvents.move + '.abcScroll', this.handleMove.bind(this)); 
     this.$el.on(touchEvents.end + '.abcScroll', this.handleEnd.bind(this));
+    
+    // Add click events
     this.$el.on('click' + '.abcScroll', this.handleClick.bind(this));
+    
+    // Add scroll event
     this.$content.on('scroll' + '.abcScroll', this.handleScroll.bind(this));
 
     $(this.$content).find('[data-alpha]').each(function() {
-      var $this = $(this),
-          _data = $this.data(),
-          elOffset = $this.offset(),
+      var $this = $(this), 
+          _data = $this.data(), 
+          elOffset = $this.offset(), 
           containerOffset = self.$content.offset();
 
       var scrollPosition = elOffset.top - containerOffset.top;
       self.posList.push({ alpha: _data.alpha, pos: scrollPosition });
-
+      
       $(self.$el).find('button[data-alpha="' + _data.alpha + '"]').removeAttr('disabled');
     });
 
+    // Sort positions list by scroll position
     this.posList.sort(function(a, b) {
       return a.pos - b.pos;
     });
   }
 
-  AbcScroll.prototype.handleStart = function(e) {
-    e.preventDefault();
+  AbcScroll.prototype.handleStart = function(e) { 
+    e.preventDefault();  
     this.setScroll(getTarget(e.originalEvent));
   }
 
-  AbcScroll.prototype.handleMove = function(e) {
-    e.preventDefault();
+  AbcScroll.prototype.handleMove = function(e) { 
+    e.preventDefault();  
     this.setScroll(getTarget(e.originalEvent));
   }
 
-  AbcScroll.prototype.handleEnd = function(e) {
-    e.preventDefault();
+  AbcScroll.prototype.handleEnd = function(e) { 
+    e.preventDefault();  
     this.setScroll(getTarget(e.originalEvent));
     $(this.$el).find('button').removeClass('current');
   }
@@ -98,6 +94,7 @@
   AbcScroll.prototype.handleScroll = function() {
     var self = this;
     var scrollTop = this.$content.scrollTop();
+
     $.each(this.posList, function(index, posObj) {
       if (scrollTop >= posObj.pos && (index + 1 === self.posList.length || scrollTop < self.posList[index + 1].pos)) {
         $(self.$el).find('button').removeClass('current');
@@ -110,17 +107,20 @@
     if (!target) {
       return;
     }
-    var $target = $(target),
-        _data = $target.data(),
+    
+    var $target = $(target), 
+        _data = $target.data(), 
         posObj = this.posList.find(function(item) { return item.alpha === _data.alpha; });
+    
     if (posObj && posObj.pos !== this._oldPos) {
       $(this.$el).find('button').removeClass('current');
       $target.addClass('current');
+     
       this.$content.scrollTop(posObj.pos);
-      this._oldPos = posObj.pos;
-    }
+      this._oldPos = posObj.pos; 
+    }   
   }
-
+  
   $.fn[pluginName] = function(options) {
      return this.each(function () {
        if (!$.data(this, pluginName)) {
@@ -129,9 +129,8 @@
      });
   }
 
-  $(document).on('ready', function() {
-    $('.js-abcscroll').abcScroll();
-    isotopeInit();
-  });
-
 })(jQuery, window, document);
+
+$(document).on('ready', function() {
+  $('.js-abcscroll').abcScroll();
+});
