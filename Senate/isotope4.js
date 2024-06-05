0de8bc628b2cb04a5b5dddf56d273070599f1391
gloverd <glover.d@gmail.com>
2024-06-05 09:53:16 -0400

    fixed $$

--- Senate/isotope4.js
+++ Senate/isotope4.js
@@ -70,6 +70,42 @@ $(document).ready(function() {
     }
 
 
+   // System to Click on Chips for Senate Groups
+   $('div.card-body p.chip.description').on('click', function(event) {
+    var $chippy = $(event.currentTarget);
+    var filterGroup = $chippy.attr('data-filter-group');
+    var filterData = $chippy.attr('data-filter');
+
+    console.log("Triggered Filter by clicking on CHIP:", filterGroup, " with ", filterData);
+
+    //update Chippy Color
+    var $matchChippy = $("[data-filter-group='" + filterGroup + "']");
+    $matchChippy.find('.selected').removeClass('selected');
+
+      // Check if there is already a value for this $filterGroup
+      if (filters[filterGroup] === filterData) {
+        filters[filterGroup] = "";
+        console.log("Filter ", filters[filterGroup], "removed for group:", filterGroup);
+        $chippy.removeClass('selected');
+    } else {
+        // Otherwise, apply the filter
+        filters[filterGroup] = filterData;
+        console.log("Filter applied for group:", filterGroup, filterData);
+        
+        $chippy.addClass('selected');
+    }
+
+    // Combine filters and send to isotope
+    var filterValue = concatValues(filters);
+    $gallery.isotope({ filter: filterValue });
+    $gallery.isotope({ sortBy: global_sort_class });
+
+    //update Chippy Color
+    var $matchChippy = $("[data-filter-group='" + filterGroup+ "']");
+    $matchChippy.find('.selected').removeClass('selected');
+
+});
+
     
     // System to Click on Chips
 /*
@@ -92,43 +128,6 @@ $(document).ready(function() {
 
     });
 */
-
-   // System to Click on Chips for Senate Groups
-   $('div.card-body p.chip.description').on('click', function(event) {
-    var $chippy = $(event.currentTarget);
-    var $filterGroup = $chippy.attr('data-filter-group');
-    var $filterData = $chippy.attr('data-filter');
-
-    console.log("Triggered Filter by clicking on CHIP:", filterGroup, " with ", $filterData);
-
-    //update Chippy Color
-    var $matchChippy = $("[data-filter-group='" + filterGroup + "']");
-    $matchChippy.find('.selected').removeClass('selected');
-
-      // Check if there is already a value for this filterGroup
-      if (filters[filterGroup] === $filterData) {
-        filters[filterGroup] = "";
-        console.log("Filter ", filters[filterGroup], "removed for group:", filterGroup);
-        $chippy.removeClass('selected');
-    } else {
-        // Otherwise, apply the filter
-        filters[filterGroup] = $filterData;
-        console.log("Filter applied for group:", filterGroup, $filterData);
-        
-        $chippy.addClass('selected');
-    }
-
-    // Combine filters and send to isotope
-    var filterValue = concatValues(filters);
-    $gallery.isotope({ filter: filterValue });
-    $gallery.isotope({ sortBy: global_sort_class });
-
-    //update Chippy Color
-    var $matchChippy = $("[data-filter-group='" + filterGroup+ "']");
-    $matchChippy.find('.selected').removeClass('selected');
-
-});
-
 
 
     // Function to add IDs to the first instance of each letter
