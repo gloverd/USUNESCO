580901d11b51941a7b73d6f107b1bf888623b69a
gloverd <glover.d@gmail.com>
2024-06-06 07:08:26 -0400

    added #sorts > to button group

--- Senate/isotope4.js
+++ Senate/isotope4.js
@@ -157,8 +157,8 @@ $(document).ready(function() {
 
 
         
-    // Change is-checked class on buttons
-    $('.button-group').each(function(i, buttonGroup) {
+    // Change is-checked class on header buttons buttons
+    $('#sorts > .button-group').each(function(i, buttonGroup) {
         var $buttonGroup = $(buttonGroup);
         $buttonGroup.on('click', 'button', function(event) {
             $buttonGroup.find('.is-checked').removeClass('is-checked');
